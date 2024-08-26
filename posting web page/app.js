7687e88ad1f845b362b8cbf62dead1c42d230f3c
BushraTariq1624 <tbushra211@gamil.com>
2024-08-26 21:09:40 +0500

    JavaScript Projrcts

--- posting web page/app.js
+++ posting web page/app.js
@@ -1,15 +1,15 @@
 function post() {
-  var username = document.getElementById("username").value
+  var username = document.getElementById("username")
   var text = document.getElementById("text")
 
   var cards = document.getElementById("cards")
   cards.innerHTML += `<div class="card">
   <div class="card-header">
-    <p>${text.value}<p>
+    <p><p>
   </div>
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <h5 class="card-title">${username.value}</h5>
+    <p class="card-text">${text.value}</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>`
