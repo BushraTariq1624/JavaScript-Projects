0e63489fd9f0a60fc329485ef2727956935c9f2d
BushraTariq1624 <tbushra211@gamil.com>
2024-09-22 18:05:14 +0500

    JS Work

--- posting web page/app.js
+++ posting web page/app.js
@@ -5,12 +5,11 @@ function post() {
   var cards = document.getElementById("cards")
   cards.innerHTML += `<div class="card">
   <div class="card-header">
-    <p><p>
+    <p>Your Post<p>
   </div>
   <div class="card-body">
     <h5 class="card-title">${username.value}</h5>
     <p class="card-text">${text.value}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>`
 }
